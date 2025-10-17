c9dfc3ba53268ebfbe9e2ffe1e510b260a2bf1f6
Matt Ashton <mattashton@google.com>
2025-10-17 22:05:53 +0000

    initial set-up and concept designing of game/app

---
+++ matchbox.py
@@ -0,0 +1,19 @@
+# Developed by Matt Ashton (Google Public Sector) and Tyler Witte, PhD (Purdue Fort Wayne).
+#build a game of naughts and crosses (tic-tac-toe) with matchboxes, pebbles, and reinforcment learning (via rewards and punishments)
+
+"""
+High-level concepts
+1)Game
+    -Board
+    -X-O piece
+    -Turn Control
+    -Available Moves
+    -Win Condition Logic
+    -Game State
+    -Display (GUI)
+2)Score/Win Condition
+3)AI
+
+i/3 - rows
+i%3 - cols 
+"""
