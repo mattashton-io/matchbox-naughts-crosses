67a4bf74847eda1c25b19b91398429857a48ffdc
Matt Ashton <mattashton@google.com>
2025-11-06 00:44:03 +0000

    win conditions complete, check for moves still being worked out and changing turns

--- matchbox.py
+++ matchbox.py
@@ -24,7 +24,7 @@ import time
 
 
 board = [0, 1, 2, 3, 4, 5, 6, 7, 8]
-game_pieces =
+game_pieces = []
 menu = {} #choose friend or AI
 stats = {}
 settings = {}
@@ -32,10 +32,7 @@ difficulty = ["easy", "medium", "impossible"]
 start_game = {}
 gui = {}
 game_state = ["-", "-", "-", "-", "-", "-", "-", "-", "-"]
-valid_moves = []
-is_valid_move = False
-
-player = 1
+global player 
 
 
 
@@ -43,17 +40,22 @@ player = 1
 # generate empty game-state
 #loop through game state
 def allowed_moves():
-    for i in range[9]:
+    temp_array = []
+    for i in range(9):
         if game_state[i] == "-":
-            valid_moves.append([i])
+            temp_array.append(i)
+    return temp_array        
 
 
 def get_move():
 #check against valid moves
     time.sleep(3)
+    valid_moves = allowed_moves()
+    is_valid_move = False
     while not is_valid_move:
         print("your turn")
-        temp_move = input("make a move: ")
+        print("valid moves: ", valid_moves)
+        temp_move = int(input("make a move: "))
         for i in range(len(valid_moves)):
             if temp_move == valid_moves[i]:
                 is_valid_move = True
@@ -70,56 +72,77 @@ def get_move():
 
 #check for win
 def check_for_win():
+    print(game_state)
     moves = []
     #check which player whose turn it is
     if player == 1:
-        for p in len(game_state):
+        for p in range(len(game_state)):
             if game_state[p] == "X":
             #list their moves only
                 moves.append(p)
     elif player == 0:
-        for p in len(game_state):
+        for p in range(len(game_state)):
             if game_state[p] == "O":
             #list their moves only
                 moves.append(p)
     else:
-        return -1
+        return -1 #if for some reason there's an incorrect player ID entered, return -1
+    
 
-    for i in len(game_state):
-        if i//3 == 0:
-            return 0
-        elif 
-        
+    win_conditions = [0, 0, 0, 0, 0, 0, 0, 0] #list/set of 3 x horizontal , 3x vert, 2x diag
+
+    for i in range(len(moves)):
+        win_conditions[moves[i]//3] += 1
+        win_conditions[moves[i]%3 + 3] += 1
+        if moves[i]%4 == 0:
+            win_conditions[6] += 1
+        if moves[i]%2 == 0 and moves[i] != 8 and moves[i] != 0:
+            win_conditions[7] += 1
+    
+    for w in range(len(win_conditions)):
+        if win_conditions[w] == 3:
+            return 1
+    print(win_conditions)    
+    return 0
+
+
 #update turn_tracker
 def change_turn():
+    global player
     if player == 1:
         player = 0
     else:
         player = 1
 
 def init_game():
-    return 1
+    global player
+    player = 1
+    
 
-def update_game_state():
+def update_game_state(i, c):
+    game_state[i] = c 
     return 1
 
 #clear board
-
+            
+    
 #PLay state
 def play():
     init_game()
-    while True:
-        #check which moves are valid
-        allowed_moves()
-        
+    while check_for_win() != 1:
+        if player == 1:
+            c = "X"
+        else:
+            c = "O"
+#       update the player 
+        change_turn()
+
         #get move from AI
-        get_move()
+        move = get_move()
         
-        update_game_state()
+        update_game_state(move, c)
 
         #check for win
         check_for_win()
-
-        #update the player 
-        change_turn()
     
+play()
